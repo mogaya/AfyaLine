f4d4cde08c1749f0e1bb7e080437685141270102
mogaya <mogayainnocent@gmail.com>
2025-04-24 15:31:23 +0300

    added client viewset

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Client
+from .serializers import ClientSerializer
 
 # Create your views here.
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all().order_by('-date_registered')
+    serializer_class = ClientSerializer
+     

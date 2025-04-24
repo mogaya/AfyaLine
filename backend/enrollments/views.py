459f18b2eaf18ac4455f28d148b6b972e9836754
mogaya <mogayainnocent@gmail.com>
2025-04-24 17:16:04 +0300

    added enrollment viewset

--- backend/enrollments/views.py
+++ backend/enrollments/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Enrollment
+from .serializers import EnrollmentSerializer
 
 # Create your views here.
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all().order_by('-enrollment_date')
+    serializer_class = EnrollmentSerializer

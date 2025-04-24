d6dd69c84b9eeae11a15fcf18cfdc899882bf093
mogaya <mogayainnocent@gmail.com>
2025-04-24 16:00:19 +0300

    added program models

--- backend/programs/models.py
+++ backend/programs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Program(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name

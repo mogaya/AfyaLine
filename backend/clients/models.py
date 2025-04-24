dcf2baf96e7f5c91ec4d044a2b0688db9397823e
mogaya <mogayainnocent@gmail.com>
2025-04-24 15:29:20 +0300

    added client model

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField()
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # return super().__str__()
+        return self.full_name

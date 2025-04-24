4230ea5b25bba657c92d8bb181761330e889f86a
mogaya <mogayainnocent@gmail.com>
2025-04-24 16:01:07 +0300

    added program viewset

--- backend/programs/views.py
+++ backend/programs/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Program
+from .serializers import ProgramSerializer
 
 # Create your views here.
+class ProgamViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all().order_by('-start_date')
+    serializer_class = ProgramSerializer

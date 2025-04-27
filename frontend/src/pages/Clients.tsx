d5e1a40af7922b0dacf58943122c6ee666a0ce4f
mogaya <mogayainnocent@gmail.com>
2025-04-27 16:16:01 +0300

    added content - table, add client btn ...

--- frontend/src/pages/Clients.tsx
+++ frontend/src/pages/Clients.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import { UserRoundPlus } from "lucide-react";
+import { Link } from "react-router";
+import { useClients } from "../hooks/UseClients";
+import ClientTable from "../components/Client/ClientTable";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  const { clients } = useClients();
+  return (
+    <>
+      <Flex
+        justify={"space-between"}
+        align={{
+          base: "start",
+          md: "center",
+        }}
+        direction={{
+          base: "column",
+          md: "row",
+        }}
+        mb={6}
+        gap={{ base: 4, md: 0 }}
+      >
+        {" "}
+        <Heading size={"lg"}>Client List</Heading>
+        <Button
+          as={Link}
+          to={"/client/add"}
+          leftIcon={<UserRoundPlus size={18} />}
+          colorScheme="green"
+        >
+          Register New Client
+        </Button>
+      </Flex>
+      <ClientTable clients={clients}></ClientTable>
+    </>
+  );
 };
 
 export default Clients;

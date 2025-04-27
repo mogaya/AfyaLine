fedc35e759e3ec1180009f32ba590462279ab285
mogaya <mogayainnocent@gmail.com>
2025-04-27 04:26:08 +0300

    added router provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
+import router from "./routes/AppRoutes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router}></RouterProvider>
     </ChakraProvider>
   </StrictMode>
 );
